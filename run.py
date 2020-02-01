7e7922458559b904cd80f3e045f285d5b686ef73
Bradley Grantham <bgrantham@channel4.co.uk>
2020-02-01 18:29:02 +0000

    :art: Update loading to be done purely from keypoints path

--- run.py
+++ run.py
@@ -62,7 +62,7 @@ for subject in dataset.subjects():
             anim['positions_3d'] = positions_3d
 
 print('Loading 2D detections...')
-keypoints = np.load('data/data_2d_' + args.dataset + '_' + args.keypoints + '.npz', allow_pickle=True)
+keypoints = np.load(args.keypoints + '.npz', allow_pickle=True)
 keypoints_metadata = keypoints['metadata'].item()
 keypoints_symmetry = keypoints_metadata['keypoints_symmetry']
 kps_left, kps_right = list(keypoints_symmetry[0]), list(keypoints_symmetry[1])
